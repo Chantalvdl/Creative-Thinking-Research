364132743558008daad6226ae67a37d6cb7f002d
chantalvdluijtgaarden <chantalvdluijtgaarden@live.nl>
2019-04-11 15:53:24 +0200

    Added text output and main method

--- RRtest.py
+++ RRtest.py
@@ -4,3 +4,42 @@ import pandas as pd
 
 # Authors:
 # Chantal van de Luijtgaarden
+
+
+def data():
+    data = pd.read_csv('adjusted dataset.csv', delimiter=';')
+    # for i in range(1, len(data["gender"]) - 1):
+    #     if (data["gender"][i]) == 1:
+    #         print(data["gender"][i])
+    return data
+
+
+# Race and Religion
+def RRtest(data):
+    result = True
+    return result
+
+
+# Perceiving Yourself and Others Match
+def PYOMtest(data):
+    result = True
+    return result
+
+
+# Correlation Interest and Match Count
+def CIMCtest(data):
+    result = True
+    return result
+
+
+def main():
+    dataset = data()
+    rrresult = RRtest(dataset)
+    pyomresult = PYOMtest(dataset)
+    cimcresult = CIMCtest(dataset)
+    print(rrresult, file=open("merged.txt", "a"))
+    print(pyomresult, file=open("merged.txt", "a"))
+    print(cimcresult, file=open("merged.txt", "a"))
+
+
+main()
