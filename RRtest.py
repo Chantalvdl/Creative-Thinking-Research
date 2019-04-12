6fbee5077e701593086a545a1435f36aba35dc64
chantalvdluijtgaarden <chantalvdluijtgaarden@live.nl>
2019-04-12 09:37:39 +0200

    race and religion test

--- RRtest.py
+++ RRtest.py
@@ -16,7 +16,32 @@ def data():
 
 # Race and Religion
 def RRtest(data):
-    result = True
+    pos_race_pos_mat = 0
+    neg_race_neg_mat = 0
+    neg_race_pos_mat = 0
+    pos_race_neg_mat = 0
+    result = []
+
+    for i in range(1,len(data["samerace"])):
+        if (data["samerace"][i] == 1) and (data["match"][i]== 1):
+            pos_race_pos_mat += 1
+        elif (data["samerace"][i] == 0) and (data["match"][i] == 0):
+            neg_race_neg_mat += 1
+        elif (data["samerace"][i] == 0) and (data["match"][i] == 1):
+            neg_race_pos_mat += 1
+        elif (data["samerace"][i] == 1) and (data["match"][i] == 0):
+            pos_race_neg_mat += 1
+
+    result.append("PRPM  Result:")
+    result.append(pos_race_pos_mat)
+    result.append("NRNM  Result:")
+    result.append(neg_race_neg_mat)
+    result.append("NRPM  Result:")
+    result.append(neg_race_pos_mat)
+    result.append("PRNM  Result:")
+    result.append(pos_race_neg_mat)
+
+    print(result)
     return result
 
 
